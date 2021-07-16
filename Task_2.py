b989d8bd54a252fc70c6bb7626e8214ac426ace5
Himezon <61060252+Himezon@users.noreply.github.com>
2021-07-16 21:40:10 +0300

    Delete Task_2.py

--- Task_2.py
+++
@@ -1,9 +0,0 @@
-def f(a, b, k):
-    if a > b or a == 222:
-        return 0
-    if a == b:
-        return 1
-    if a < b:
-        s = str(a)+str(a**2 % 3)
-        return f(a + 1, b, k + 1) + f(a + (a - 1) - k % 16, b, k + 1) + f(int(s), b, k + 1)
-print(f(123, 321, 0))
