bc822a56b468d654ec6f8903f2db8a520df84f05
Himezon <61060252+Himezon@users.noreply.github.com>
2021-07-16 21:40:16 +0300

    Delete Task_3.py

--- Task_3.py
+++
@@ -1,8 +0,0 @@
-def f(a, b):
-    if a < b or a == 19:
-        return 0
-    if a == b:
-        return 1
-    if a > b:
-        return f(a - 1, b) + f(a - 3, b) + f(a - 11, b)
-print(f(32,25)*f(25,16))
