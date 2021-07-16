9ce52eebacd9aec1267180462a48d54182e676a9
Himezon <61060252+Himezon@users.noreply.github.com>
2021-07-16 21:40:21 +0300

    Delete Task_4.py

--- Task_4.py
+++
@@ -1,8 +0,0 @@
-def f(a, b):
-    if a > b or a == 220:
-        return 0
-    if a == b:
-        return 1
-    if a < b:
-        return f(a + 1, b) + f(a * 2, b) + f(int(str(a)+str(a)[-1:]), b)
-print(f(3, 110) * f(110, 333))
